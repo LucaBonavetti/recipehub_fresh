28e50ea0d7560bfd74a1d5fe821954198b4ac0d7
LucaBonavetti <161732253+LucaBonavetti@users.noreply.github.com>
2025-10-18 22:58:35 +0200

    added prisma

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './health.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { DbController } from './db.controller';
 
 @Module({
   imports: [],
-  controllers: [HealthController],
+  controllers: [HealthController, DbController],
+  providers: [PrismaService],
 })
 export class AppModule {}
